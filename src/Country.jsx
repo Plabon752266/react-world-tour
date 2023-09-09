b1e6e19ceb6ddf54c75481dde7837f7231b9017b
Plabon <plabonchowdhury007@gmail.com>
2023-09-09 22:23:56 +0600

    conditional rendering

--- src/Country.jsx
+++ src/Country.jsx
@@ -1,8 +1,15 @@
 
+import { useEffect, useState } from 'react';
 import './Country.css'
 const Country = ({country}) => {
 
     // destructuring the object of api
+
+    const [visited, setVisited] = useState(false)
+
+    const handleClick = () =>{
+        setVisited(true)
+    }
 
     const {name, flags, region, capital, population} = country
     return (
@@ -12,6 +19,8 @@ const Country = ({country}) => {
             <p> Region: {region}  </p>
             <p>Capital City : {capital} </p>
             <p>Populaton of the country: {population} </p>
+            <button onClick={handleClick} className="btn">Button</button>
+            {visited && 'i have already visited'}
         </div>
     );
 };
