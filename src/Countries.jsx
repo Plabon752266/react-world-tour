ed16759cf7ce6858a20805f6bcbdd8571413c69b
Plabon <plabonchowdhury007@gmail.com>
2023-09-09 22:09:04 +0600

    grid layout create

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -2,22 +2,25 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Country from "./Country";
 import './country.css'
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountries(data));
+            .then(res => res.json())
+            .then(data => setCountries(data));
 
-    },[])
+    }, [])
     return (
         <div>
             <h3>Countries: {countries.length} </h3>
-            {
-                countries.map(country => <Country country={country} key={country.cca3} ></Country>)
-            }
+            <div className="countries-container">
+                {
+                    countries.map(country => <Country country={country} key={country.cca3} ></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/Country.jsx
+++ src/Country.jsx
@@ -1,13 +1,17 @@
 
 import './Country.css'
 const Country = ({country}) => {
-    const {name, flags, region} = country
+
+    // destructuring the object of api
+
+    const {name, flags, region, capital, population} = country
     return (
         <div className="country">
             <h3>country Name : {name?.common}</h3>
-            
             <img src={flags?.png} alt="" />
             <p> Region: {region}  </p>
+            <p>Capital City : {capital} </p>
+            <p>Populaton of the country: {population} </p>
         </div>
     );
 };
